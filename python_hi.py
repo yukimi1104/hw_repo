abb9ae1e6189f88f0ff097310227399f89476961
yukim <yukimi1999@outlook.com>
2026-02-19 09:53:04 +0100

    Main hi

--- python_hi.py
+++ python_hi.py
@@ -3,3 +3,6 @@ import random
 print("Hello world")
 if random.random() > 0.5:
     print("Again: Hello world!!")
+print("Hello from  main!")
+if random.random() >0.5:
+    print("Again: Hello world!!")
